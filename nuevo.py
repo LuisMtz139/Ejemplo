6b5a3c14dd957b35d6659badf0b4447210b80588
LuisMtz139 <LuisMartinezMA12@gmail.com>
2025-01-11 19:17:39 -0600

    Pruebas 2

--- nuevo.py
+++ nuevo.py
@@ -1,13 +1,13 @@
-    dict = {'c': 1, 'b': 3, 'a': 2}
+dict = {'c': 1, 'b': 3, 'a': 2}
 
-    "me gusta los gatos"
+"me gusta los gatos"
 
-    def nuevo():
-        return 1
+def nuevo():
+    return 1
 
 
-    def nuevo2():
-        return 23
+def nuevo2():
+    return 23
 
 
 
