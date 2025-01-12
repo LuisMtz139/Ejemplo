18683f8de05c2cf0eeec50fd3ef94c24260947b4
Miller V <vidallopez0726@gmail.com>
2025-01-11 19:12:48 -0600

    Pruebas

--- nuevo.py
+++ nuevo.py
@@ -1,11 +1,11 @@
-dict = {'a': 1, 'b': 2, 'c': 3}
+    dict = {'c': 1, 'b': 3, 'a': 2}
 
-def nuevo():
-    return 1
+    def nuevo():
+        return 1
 
 
-def nuevo2():
-    return 23
+    def nuevo2():
+        return 23
 
 
 
