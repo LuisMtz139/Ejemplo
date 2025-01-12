4c439d0daae56604a625c191ea23abec04f22c52
Miller V <vidallopez0726@gmail.com>
2025-01-11 19:32:15 -0600

    Pruebass

--- nuevo.py
+++ nuevo.py
@@ -1,18 +1,16 @@
-
 nuevo = {
-    
-    'a': 1,
-    'b': 2,
+    'a': 10,   
+    'b': 20,  
     'c': 3,
     'd': 4,
     'e': 5,
     'f': 6,
 }
 
-"me gusta los gatos"
+"me gustan los gatos negros" 
 
 def nuevo1(nuevo):
     for data1 in nuevo:
-        print(data1)
+        print(f"Valor: {nuevo[data1]}")
 
 nuevo1(nuevo)
