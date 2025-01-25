2d3c538c6dea42ed3acd057c8300b26ebe51b096
LuisMtz139 <LuisMartinezMA12@gmail.com>
2025-01-25 00:17:15 -0600

    Todo

--- nuevo.py
+++ nuevo.py
@@ -1,16 +1,5 @@
-nuevo = {
-    'a': 12,   
-    'li': 0,  
-    'c': 10000,
-    'd': 0,
-    'ño': 17,
-    'f': 0,
+data : {
+    "nombre" : "Juan",
+    "edad" : 30
+    
 }
-
-"i like really  pets, i lerned english whit dualingo" 
-
-def nuevo1(nuevo):
-    for data1 in nuevo:
-        print(f"Key -> {data1}: {nuevo[data1]}")  # Print modificado
-
-nuevo1(nuevo)
